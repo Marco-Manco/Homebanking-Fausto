6989998be1d21fd7000ca4f3b1ac03073386766f
Marco-Manco <mmanco@fi.uba.ar>
2022-08-10 14:39:51 -0300

    se borra una linea de codigo que no debia ir en clientLoanRepository

--- src/main/java/com/mindhub/homebanking/repositories/ClientLoanRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/ClientLoanRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientLoanRepository extends JpaRepository<ClientLoan, Long> {
 
-    interface CardRepository extends JpaRepository<Card, Long> {
-    }
+
 }
